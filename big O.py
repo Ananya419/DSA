7d06c0b81146d281d0095b8f724d15bb512561e0
Ananya419 <shahiananya381@gmail.com>
2025-09-06 14:29:19 +0530

    update today's work

--- big O.py
+++ big O.py
@@ -11,3 +11,24 @@ def items(n):
         print(i)
 
 items(10)
+
+
+# Drop constants
+def items(n):
+    for i in range(n):
+        print(i)
+
+    for j in range(n):
+        print(j)
+
+items(5)
+
+
+
+# O(n^2) time complexity
+def items(n):
+    for i in range (n):
+        for j in range (n):
+            print(i,j)
+
+items(8)
