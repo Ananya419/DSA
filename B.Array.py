77bced8285a019dfceb29ebb80e453ab776ef3d8
Ananya419 <shahiananya381@gmail.com>
2025-09-17 21:44:21 +0530

    update recent task

--- B.Array.py
+++ B.Array.py
@@ -52,3 +52,13 @@ def traverseArray(array):
 
 traverseArray(arr1)
 traverseArray(arr2)
+
+
+# ACCESSING AN ELEMENT OF AN ARRAY
+from array import *
+arr1 = array('i', [1,2,3,4,5,6])
+def accessElement(array, index):
+    if index > len(array):
+        print('There is not any element in this index')
+    else:
+        print(array[index])    
